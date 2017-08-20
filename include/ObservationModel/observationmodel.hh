aef425f89c7a326cd3299c685e5f8119955c6146
Sander van Dijk <sgvandijk@gmail.com>
2017-08-20 18:26:53 +0100

    Start of observation model

---
+++ include/ObservationModel/observationmodel.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Eigen/Core>
+
+namespace rbest
+{
+
+  template<typename VECS_TYPE, int STATE_DIM, int OBSERVATION_DIM>
+  class ObservationModel
+  {
+  public:
+    using StateVector       = Eigen::Matrix<VECS_TYPE, STATE_DIM, 1>;
+    using ObservationVector = Eigen::Matrix<VECS_TYPE, OBSERVATION_DIM, 1>;
+
+  public:
+    virtual ObservationVector observe(StateVector const& state);
+  };
+  
+}
