74efe1b1c395ebe9546dc21a4323caecb889c1be
Sander van Dijk <sgvandijk@gmail.com>
2017-11-12 20:49:57 +0000

    Run filter and show estimate in flyer

--- examples/flyer.cc
+++ examples/flyer.cc
@@ -34,28 +34,52 @@ int main(int argc, char const** argv)
     return -1;
   }
   
-  auto quit = false;
-  auto event = SDL_Event{};
-
   // State consists of height and vertical velocity
-  auto realState = Eigen::Vector3d{0.0, 0.0, 0.0};
-
-  auto stateHistory = std::list<Eigen::Vector3d>{};
-  
   // Flyer moves horizontally at constant speed, which is not filtered
+  auto realState = Eigen::Vector3d{0.0, 0.0, 0.0};
+  auto stateHistory = std::list<Eigen::Vector3d>{};  
+  auto estHistory = std::list<Eigen::Vector3d>{};  
   auto xSpeed = 5.0;
 
   auto randGen = std::default_random_engine{};
-  
-  auto systemNoiseDist = std::normal_distribution<double>{0., 0.1};
+
+  auto systemNoiseStd = 0.1;
+  auto systemNoiseVar = systemNoiseStd * systemNoiseStd;
+  auto systemNoiseDist = std::normal_distribution<double>{0., systemNoiseStd};
   auto systemNoise = [&]() { return systemNoiseDist(randGen); };
+
+  auto observationNoiseStd = 25.;
+  auto observationNoiseVar = observationNoiseStd * observationNoiseStd;
+  auto observationNoiseDist = std::normal_distribution<double>{0., observationNoiseStd};
+  auto observationNoise = [&]() { return observationNoiseDist(randGen); };
   
+  // Filter
+  // State to be filtered consists of height and vertical velocity
+  // Only vertical velocity is controlled, and only height is observed
+  using Filter = rbest::KalmanFilter<double, 2, 1, 1>;
+  auto filter = Filter{};
+
+  auto systemModel = Filter::SystemModelType{};
+  systemModel.setTransitionMatrix((Filter::SystemModelType::TransitionMatrix{} <<
+                                   1., 1.,
+                                   0., 1.).finished());
+  systemModel.setControlMatrix((Filter::SystemModelType::ControlMatrix{} << 0., 0.05).finished());
+  systemModel.setSystemNoiseCovar((Filter::SystemModelType::SystemNoiseCovar{} <<
+                                   systemNoiseVar, 0.,
+                                   0., systemNoiseVar).finished());
+
+  auto observationModel = Filter::ObservationModelType{};
+  observationModel.setObservationMatrix((Filter::ObservationModelType::ObservationMatrix{} << 1., 0.).finished());
+  observationModel.setObservationNoiseCovar((Filter::ObservationModelType::ObservationNoiseCovar{} << observationNoiseVar).finished());
   
+  // Visualisation details
   auto gridSize = 100;
   
+  auto quit = false;
+  auto event = SDL_Event{};
   while (!quit)
   {
-    auto yAccel = 0.;
+    auto control = 0.0;
     
     while (SDL_PollEvent(&event) != 0)
     {
@@ -72,24 +96,26 @@ int main(int argc, char const** argv)
           quit = true;
           break;
         case SDLK_UP:
-          yAccel = 0.05;
+          control = 1.;
           break;
         case SDLK_DOWN:
-          yAccel = -0.05;
+          control = -1.;
         }
         break;
       }      
     }
 
-    stateHistory.push_back(realState);
-    while (stateHistory.size() > SCREEN_WIDTH / xSpeed)
-      stateHistory.pop_front();
-    
+    auto yAccel = control * 0.1;
+
     // Update state
     realState.x() += xSpeed;
     realState.y() += realState.z() + systemNoise();
     realState.z() += yAccel + systemNoise();
 
+    // Run filter
+    filter.predict(systemModel, Eigen::Matrix<double, 1, 1>{control});
+    filter.update(observationModel, Eigen::Matrix<double, 1, 1>{realState.y() + observationNoise()});
+    
     // render
 
     // Clear screen
@@ -110,15 +136,27 @@ int main(int argc, char const** argv)
 
     // Draw history
     SDL_SetRenderDrawColor(renderer, 0x34, 0x65, 0xA4, 0xFF );
-    auto iter = stateHistory.begin();
-    auto hs1 = *iter++;
-    for ( ; iter != stateHistory.end(); ++iter)
+    auto siter = stateHistory.begin();
+    auto hs1 = *siter++;
+    for ( ; siter != stateHistory.end(); ++siter)
     {
-      auto hs2 = *iter;
+      auto hs2 = *siter;
       SDL_RenderDrawLine(renderer,
                          700 + hs1.x() - realState.x(), 300 - hs1.y() + realState.y(),
                          700 + hs2.x() - realState.x(), 300 - hs2.y() + realState.y());
       hs1 = hs2;
+    }
+    
+    SDL_SetRenderDrawColor(renderer, 0x73, 0xD2, 0x16, 0xFF );
+    auto eiter = estHistory.begin();
+    auto he1 = *eiter++;
+    for ( ; eiter != estHistory.end(); ++eiter)
+    {
+      auto he2 = *eiter;
+      SDL_RenderDrawLine(renderer,
+                         700 + he1.x() - realState.x(), 300 - he1.y() + realState.y(),
+                         700 + he2.x() - realState.x(), 300 - he2.y() + realState.y());
+      he1 = he2;
     }
     
     // Draw flyer
@@ -126,7 +164,20 @@ int main(int argc, char const** argv)
     auto flyer = SDL_Rect{700 - 2, 300 - 2, 4, 4};
     SDL_RenderFillRect(renderer, &flyer);
     
+    // Draw estimated state
+    SDL_SetRenderDrawColor(renderer, 0x8A, 0xE2, 0x34, 0xFF );
+    auto stateEst = filter.getState();
+    auto flyerEst = SDL_Rect{700 - 2, 300 - stateEst(0) + realState.y() - 2, 4, 4};
+    SDL_RenderFillRect(renderer, &flyerEst);
 
+    // Store history
+    stateHistory.push_back(realState);
+    while (stateHistory.size() > SCREEN_WIDTH / xSpeed)
+      stateHistory.pop_front();
+    estHistory.push_back(Eigen::Vector3d{realState.x(), stateEst(0), stateEst(1)});
+    while (estHistory.size() > SCREEN_WIDTH / xSpeed)
+      estHistory.pop_front();
+    
     SDL_RenderPresent(renderer);
     SDL_UpdateWindowSurface(window);
 
